f0d19579253d26b040e5913a2844d644c6199f18
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2021-10-30 09:36:23 +0300

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -127,7 +127,7 @@ async def who_is(client, message):
 @Client.on_message(filters.command(["imdb", 'search']))
 async def imdb_search(client, message):
     if ' ' in message.text:
-        k = await message.reply('Bekle Arıyorum imdb'de')
+        k = await message.reply('Bekle Arıyorum.')
         r, title = message.text.split(None, 1)
         movies = await get_poster(title, bulk=True)
         if not movies:
