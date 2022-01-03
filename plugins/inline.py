fafc3ff528b7265f5a9d16dbe1232e5a23fcaeb7
mmagnetooooooo2 <95867923+mmagnetooooooo2@users.noreply.github.com>
2022-01-03 19:12:24 +0300

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -30,7 +30,6 @@ async def answer(bot, query):
         file_type = None
 
     offset = int(query.offset or 0)
-    reply_markup = get_reply_markup(query=string)
     files, next_offset, total = await get_search_results(string,
                                                   file_type=file_type,
                                                   max_results=10,
