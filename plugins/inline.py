d84767c4e035d59ad596f84dc5eb2cdf2e7e8b38
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-01-20 10:46:19 +0300

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -4,22 +4,22 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQue
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size
 from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
-
+ 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
-
-
+ 
+ 
 @Client.on_inline_query(filters.user(AUTH_USERS) if AUTH_USERS else None)
 async def answer(bot, query):
-    """Verilen satır içi sorgu için arama sonuçlarını göster"""
-
+    """Show search results for given inline query"""
+ 
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='Botu kullanmak için kanalıma abone olmalısınız ',
+                           switch_pm_text='Botu kullanmak için kanalıma abone olmalısınız',
                            switch_pm_parameter="subscribe")
         return
-
+ 
     results = []
     if '|' in query.query:
         string, file_type = query.query.split('|', maxsplit=1)
@@ -28,14 +28,14 @@ async def answer(bot, query):
     else:
         string = query.query.strip()
         file_type = None
-
+ 
     offset = int(query.offset or 0)
     reply_markup = get_reply_markup(query=string)
     files, next_offset, total = await get_search_results(string,
                                                   file_type=file_type,
                                                   max_results=10,
                                                   offset=offset)
-
+ 
     for file in files:
         title=file.file_name
         size=get_size(file.file_size)
@@ -55,11 +55,11 @@ async def answer(bot, query):
                 caption=f_caption,
                 description=f'Boyut: {get_size(file.file_size)}\nTür: {file.file_type}',
                 reply_markup=reply_markup))
-
+ 
     if results:
         switch_pm_text = f"{emoji.FILE_FOLDER} Sonuçlar - {total}"
         if string:
-            switch_pm_text += f" for {string}"
+            switch_pm_text += f" {string}"
         try:
             await query.answer(results=results,
                            is_personal = True,
@@ -74,13 +74,21 @@ async def answer(bot, query):
                            switch_pm_text=str(e)[:63],
                            switch_pm_parameter="error")
     else:
-        switch_pm_text = f'{emoji.CROSS_MARK} O Film Bende Yok!'
+        switch_pm_text = f'{emoji.CROSS_MARK} Sonuç yok'
         if string:
             switch_pm_text += f' for "{string}"'
-
+ 
         await query.answer(results=[],
                            is_personal = True,
                            cache_time=cache_time,
                            switch_pm_text=switch_pm_text,
                            switch_pm_parameter="okay")
-
+ 
+ 
+def get_reply_markup(query):
+    buttons = [
+        [
+            InlineKeyboardButton('Tekrar Ara', switch_inline_query_current_chat=query)
+        ]
+        ]
+    return InlineKeyboardMarkup(buttons)
