4b4310cd557d426ef6f3202fac0862f5d36842f6
mmagnetooooooo2 <95867923+mmagnetooooooo2@users.noreply.github.com>
2022-01-31 00:44:48 +0300

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -136,9 +136,9 @@ async def re_enable_chat(bot, message):
     await message.reply("Sohbet Başarıyla yeniden etkinleştirildi.")
 
 
-@Client.on_message(filters.command('stats'))
+@Client.on_message(filters.command('stats') & filters.user(ADMINS))
 async def get_ststs(bot, message):
-    rju = await message.reply('Fetching stats..')
+    rju = await message.reply('Bilgiler Alınıyor..')
     total_users = await db.total_users_count()
     totl_chats = await db.total_chat_count()
     files = await Media.count_documents()
